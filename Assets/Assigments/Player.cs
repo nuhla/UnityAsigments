71c6c52d0f5bef11d3d05cc94eb4d533c7ea0d2d
nuhlamasri@gmai.com <nuhlamasri@gmail.com>
2024-11-05 01:17:06 +0200

    Update Readme

--- Assets/Assigments/Player.cs
+++ Assets/Assigments/Player.cs
@@ -4,38 +4,22 @@ using UnityEngine;
 
 public class Player
 {
-    string playerName = "NewPlayer";
-    int health = 100;
-    static int playerCount = 0;
+    string playerName = "NewPlayer";    int health = 100;    static int playerCount = 0;
     public void InitializePlayer(string name, int initialHealth)
     {
-        playerCount++;
-        playerName = name;
-        health = initialHealth;
+        playerCount++;        playerName = name;        health = initialHealth;
     }
     public void Heal(int amount)
     {
-        if (health + amount > 100)
-        {
-            health = 100;
-        }
-        else if (health + amount < 0)
-        {
-            health = 0;
-        }
-        else
-        {
-            health += amount;
-        }
+        if (health + amount > 100)        {            health = 100;        }
+        else if (health + amount < 0)        {            health = 0;        }
+        else        {            health += amount;        }
         Debug.Log("Health is : " + health);
         Debug.Log("From Main method");
     }
     public void Heal(bool fullRestore)
     {
-        if (fullRestore)
-        {
-            health = 100;
-        }
+        if (fullRestore)        {            health = 100;        }
 
         Debug.Log("Health is : " + health);
         Debug.Log("From Overload method");
