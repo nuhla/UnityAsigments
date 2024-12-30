60533c900db9c4af36d8e00f152e2a47c87f812f
nuhlamasri@gmai.com <nuhlamasri@gmail.com>
2024-12-30 03:03:00 +0200

    Assigment 29 Part7

---
+++ Assets/Assigments/Assignment29/Part7/RecursionScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecursionScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Debug.Log(FibonacciRecursive(10));
+        Debug.Log(FibonacciIterative(30));
+    }
+
+    int FibonacciRecursive(int n)
+    {
+        return (n < 2) ? n : FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
+    }
+
+    int FibonacciIterative(int x)
+    {
+        if (x == 0) return 0;
+
+        int prev = 0;
+        int next = 1;
+        for (int i = 1; i < x; i++)
+        {
+            int sum = prev + next;
+            prev = next;
+            next = sum;
+        }
+        return next;
+    }
+    // Update is called once per frame
+
+}
