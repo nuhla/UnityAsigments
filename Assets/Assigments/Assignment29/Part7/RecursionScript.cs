83760462fca77c04b8ee83902c23bcdc0ababe0b
nuhlamasri@gmai.com <nuhlamasri@gmail.com>
2024-12-30 03:06:53 +0200

    Assigment 29 Part7

--- Assets/Assigments/Assignment29/Part7/RecursionScript.cs
+++ Assets/Assigments/Assignment29/Part7/RecursionScript.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RecursionScript : MonoBehaviour
+namespace Assigment29.part3
+{
+    public class RecursionScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -33,4 +35,5 @@ public class RecursionScript : MonoBehaviour
     }
     // Update is called once per frame
 
+}
 }
