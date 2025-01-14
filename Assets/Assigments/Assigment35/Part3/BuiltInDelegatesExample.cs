8ece89938a21aa0ee8fbbf2a8ba86fab81c97c22
nuhlamasri@gmai.com <nuhlamasri@gmail.com>
2025-01-15 01:18:32 +0200

    Assigment 35 , part 7 , part3

---
+++ Assets/Assigments/Assigment35/Part3/BuiltInDelegatesExample.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assigment35.part3
+{
+    public class BuiltInDelegatesExample : MonoBehaviour
+    {
+        Action<string> logMessage;
+        Func<int, int> square;
+
+        Predicate<int> isEven;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            logMessage = (string mess) => Debug.Log(mess);
+            logMessage("Hello");
+
+            square = (int num) => 5 * 5;
+            Debug.Log($"Square: {square(5)}");
+
+            isEven = (num) => num % 2 == 0 ? true : false;
+            Debug.Log($"Is 4 even? {isEven(4)}");
+
+        }
+
+
+    }
+}
---
+++ Assets/Assigments/Assigment35/Part7/MethodChainingExample.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+namespace Assignment35.part7
+{
+    public class MethodChainingExample : MonoBehaviour
+
+    {
+        void Start()
+        {
+            SetPosition(new Vector3(0, 1, 0))
+           .SetRotation(new Vector3(45, 0, 0))
+           .SetScale(new Vector3(2, 2, 2));
+        }
+        public MethodChainingExample SetPosition(Vector3 position)
+        {
+            transform.position = position;
+            return this;
+        }
+
+        public MethodChainingExample SetRotation(Vector3 rotation)
+        {
+            transform.rotation = Quaternion.Euler(rotation);
+            return this;
+        }
+        public MethodChainingExample SetScale(Vector3 scale)
+        {
+            transform.localScale = scale;
+            return this;
+        }
+    }
+}
