8b16955de50d6c2eccfbb1da9059bce1f8d79cbf
nuhlamasri@gmai.com <nuhlamasri@gmail.com>
2025-01-15 01:40:40 +0200

    Assigment part4-5

--- Assets/Assigments/Assigment35/Part4-5/ListMethodsExample.cs
+++ Assets/Assigments/Assigment35/Part4-5/ListMethodsExample.cs
@@ -8,11 +8,15 @@ namespace Assignment35.part4and5
 }
 public class ListMethodsExample : MonoBehaviour
 {
-
+    List<int> numbers = new List<int> { 3, 1, 4, 1, 5, 9 };
     List<int> numbers2 = new List<int> { 3, 1, 4, 1, 5, 9, 2, 6 };
     // Start is called before the first frame update
     void Start()
     {
+        numbers.Sort((x, y) => x.CompareTo(y));
+        Debug.Log(string.Join(", ", numbers));
+
+
         List<int> filteredNumbers = numbers2.FindAll((number) => number % 2 == 0);
         Debug.Log(string.Join(", ", filteredNumbers));
 
