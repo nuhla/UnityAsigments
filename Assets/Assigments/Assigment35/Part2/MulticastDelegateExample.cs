bbefda9c7c1802475fddbde4dd73788921403bac
nuhlamasri@gmai.com <nuhlamasri@gmail.com>
2025-01-16 03:17:47 +0200

    Assigment part2

---
+++ Assets/Assigments/Assigment35/Part2/MulticastDelegateExample.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public delegate void MathOperation(int number);
+public class MulticastDelegateExample : MonoBehaviour
+{
+    MathOperation operation;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        operation += DoubleNumber;
+        operation += SquareNumber;
+        operation += CubeNumber;
+        operation.Invoke(5);
+
+    }
+
+    void DoubleNumber(int number)
+    {
+        Debug.Log(number * 2);
+    }
+
+    void SquareNumber(int number)
+    {
+        Debug.Log(number * number);
+    }
+
+    void CubeNumber(int number)
+    {
+        Debug.Log(number * number * number);
+    }
+}
